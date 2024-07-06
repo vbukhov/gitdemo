a6c6c9eb8aded764aa590b6c0b8426362304308d
vbukhov <vbukhov@mail.ru>
2024-07-06 21:47:13 +0300

    FIX Greeting complete

--- Greeting.java
+++ Greeting.java
@@ -1,6 +1,6 @@
 public class Greeting{
 	@Override
 	public String toString(){
-		return "Greeting!"
+		return "Greeting!";
 	}
 }
--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello{
         public static void main(String[] args){
-		//todo need new Greeting()
-                System.out.println("Hello!");                
+		Object msg = new Greeting();
+                System.out.println(msg);                
         }
 }
