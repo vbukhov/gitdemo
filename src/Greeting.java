3a68f1b4f2cd2124fe93894d05353753172d6886
vbukhov <vbukhov@mail.ru>
2024-07-07 21:58:42 +0300

    ADD src

--- src/Greeting.java
+++ src/Greeting.java
@@ -1,3 +1,5 @@
+package src;
+
 public class Greeting{
 	@Override
 	public String toString(){
--- src/Hello.java
+++ src/Hello.java
@@ -1,3 +1,5 @@
+package src;
+
 public class Hello{
         public static void main(String[] args){
 		Object msg = new Greeting();
